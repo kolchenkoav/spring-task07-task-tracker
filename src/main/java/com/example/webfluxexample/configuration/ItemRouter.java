be5c26d4dabdc62fc43ac8a1b6b09d6752fd815f
kolchenkoav <kolchenkoav@gmail.com>
2024-02-13 19:56:50 +0300

    +

---
+++ src/main/java/com/example/webfluxexample/configuration/ItemRouter.java
@@ -0,0 +1,16 @@
+package com.example.webfluxexample.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ItemRouter {
+//    @Bean
+//    public RouterFunction<ServerResponse> itemRouters(ItemHandler itemHandler) {
+//        return RouterFunctions.route()
+//                .GET("/api/v1/functions/item", itemHandler::getAllItem)
+//                .GET("/api/v1/functions/item/{id}", itemHandler::findById)
+//                .POST("/api/v1/functions/item", itemHandler::createItem)
+//                .GET("/api/v1/functions/error", itemHandler::errorRequest)
+//                .build();
+//    }
+}
